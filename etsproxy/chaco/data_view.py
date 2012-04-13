96907122bbdfc4abf14ab60109a31c2238690292
matthias <matthias@minkowski.lbb.rwth-aachen.de>
2012-04-13 13:08:34 +0200

    ETS Proxy for complete simvisage, (not finally proven)

---
+++ etsproxy/chaco/data_view.py
@@ -0,0 +1,22 @@
+#-------------------------------------------------------------------------------
+#
+# Copyright (c) 2012
+# IMB, RWTH Aachen University,
+# ISM, Brno University of Technology
+# All rights reserved.
+#
+# This software is provided without warranty under the terms of the BSD
+# license included in the Spirrid top directory "licence.txt" and may be
+# redistributed only under the conditions described in the aforementioned
+# license.
+#
+# Thanks for using Simvisage open source!
+#
+#-------------------------------------------------------------------------------
+
+# ETS 3
+try:
+    from enthought.chaco.data_view import *
+# ETS 4
+except ImportError:
+    from chaco.data_view import *
---
+++ etsproxy/traits/ui/wx/basic_editor_factory.py
@@ -0,0 +1,22 @@
+#-------------------------------------------------------------------------------
+#
+# Copyright (c) 2012
+# IMB, RWTH Aachen University,
+# ISM, Brno University of Technology
+# All rights reserved.
+#
+# This software is provided without warranty under the terms of the BSD
+# license included in the Spirrid top directory "licence.txt" and may be
+# redistributed only under the conditions described in the aforementioned
+# license.
+#
+# Thanks for using Simvisage open source!
+#
+#-------------------------------------------------------------------------------
+
+try:
+# ETS 3
+    from enthought.traits.ui.wx.basic_editor_factory import *
+# ETS 4
+except ImportError:
+    from traitsui.wx.basic_editor_factory import *
